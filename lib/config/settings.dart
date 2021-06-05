9023e600b3a979d4fa207948d0f3ee24647a7d5d
Gabriel Claret <gclaret@outlook.com>
2021-06-05 18:08:55 -0300

    add new variable for web api call

--- lib/config/settings.dart
+++ lib/config/settings.dart
@@ -1,3 +1,4 @@
 class Settings{
-  static const String apiUrl = '192.168.0.36:8000';
+  static const String apiUrl = '192.168.0.36:8000'; //for mobile
+  //static const String apiUrl = '10.0.2.2:8000'; //for web
 }
