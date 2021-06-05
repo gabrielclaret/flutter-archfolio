4bf78c3e40bcaba6166d0643f6e9f88f278a50a9
Gabriel Claret <gclaret@outlook.com>
2021-06-05 18:10:50 -0300

    update profile screen to update user with an api call

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_archfolio/config/palette.dart';
+import 'package:flutter_archfolio/config/settings.dart';
 import 'package:flutter_archfolio/data/data.dart';
 import 'package:flutter_archfolio/model/models.dart';
 import 'package:flutter_archfolio/screens/edit_profile_screen.dart';
@@ -32,117 +34,134 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.scaffold,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            brightness: Brightness.light,
-            backgroundColor: Palette.cardTheme,
-            title: Text(
-              'profile',
-              key: const Key('profileScreenText'),
-              style: const TextStyle(
-                color: mainTheme,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: -0.9,
-              ),
-            ),
-            centerTitle: false,
-            floating: true,
-            actions: [
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                child: IconButton(
-                  key: const Key('editProfileButton'),
-                  icon: Icon(Icons.edit),
-                  iconSize: 25.0,
+      body: RefreshIndicator(
+        onRefresh: _pullRefresh,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              brightness: Brightness.light,
+              backgroundColor: Palette.cardTheme,
+              title: Text(
+                'profile',
+                key: const Key('profileScreenText'),
+                style: const TextStyle(
                   color: mainTheme,
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EditProfileScreen(
-                        loggedUser: loggedUser,
-                      ),
-                    ),
-                  ),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -0.9,
                 ),
               ),
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: ProfileCard(
-              user: loggedUser,
-              isMiniature: false,
+              centerTitle: false,
+              floating: true,
+              actions: [
+                Container(
+                  margin: const EdgeInsets.all(6.0),
+                  child: IconButton(
+                    key: const Key('editProfileButton'),
+                    icon: Icon(Icons.edit),
+                    iconSize: 25.0,
+                    color: mainTheme,
+                    onPressed: () => _navigateAndDisplaySelection(context),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-            sliver: SliverToBoxAdapter(
-              child: Card(
-                color: Palette.cardTheme,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'Posts',
-                          style: const TextStyle(
-                            color: mainTheme,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: -0.8,
+            SliverToBoxAdapter(
+              child: ProfileCard(
+                user: loggedUser,
+                isMiniature: false,
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+              sliver: SliverToBoxAdapter(
+                child: Card(
+                  color: Palette.cardTheme,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'Posts',
+                            style: const TextStyle(
+                              color: mainTheme,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: -0.8,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    !_isTimeline
-                        ? IconButton(
-                            icon: Icon(Icons.dehaze),
-                            iconSize: 25.0,
-                            color: mainTheme,
-                            onPressed: () {
-                              setState(() {
-                                _isTimeline = true;
-                              });
-                            },
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.grid_view),
-                            iconSize: 25.0,
-                            color: mainTheme,
-                            onPressed: () {
-                              setState(() {
-                                _isTimeline = false;
-                              });
-                            },
-                          ),
-                  ],
+                      !_isTimeline
+                          ? IconButton(
+                              icon: Icon(Icons.dehaze),
+                              iconSize: 25.0,
+                              color: mainTheme,
+                              onPressed: () {
+                                setState(() {
+                                  _isTimeline = true;
+                                });
+                              },
+                            )
+                          : IconButton(
+                              icon: Icon(Icons.grid_view),
+                              iconSize: 25.0,
+                              color: mainTheme,
+                              onPressed: () {
+                                setState(() {
+                                  _isTimeline = false;
+                                });
+                              },
+                            ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          // loggedUser.posts.isEmpty
-          //     ? SliverToBoxAdapter(
-          //         child: Card(
-          //           child: Padding(
-          //             padding: const EdgeInsets.symmetric(vertical: 20.0),
-          //             child: Center(
-          //                 child: Text(
-          //               "User doesn't have posts yet.",
-          //               style: const TextStyle(
-          //                 color: Colors.grey,
-          //                 fontSize: 15.0,
-          //                 letterSpacing: -0.8,
-          //               ),
-          //             )),
-          //           ),
-          //         ),
-          //       )
-          //     : _showPosts(loggedUser, _isTimeline),
-        ],
+            // loggedUser.posts.isEmpty
+            //     ? SliverToBoxAdapter(
+            //         child: Card(
+            //           child: Padding(
+            //             padding: const EdgeInsets.symmetric(vertical: 20.0),
+            //             child: Center(
+            //                 child: Text(
+            //               "User doesn't have posts yet.",
+            //               style: const TextStyle(
+            //                 color: Colors.grey,
+            //                 fontSize: 15.0,
+            //                 letterSpacing: -0.8,
+            //               ),
+            //             )),
+            //           ),
+            //         ),
+            //       )
+            //     : _showPosts(loggedUser, _isTimeline),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _pullRefresh() async {
+    var dio = Dio();
+    dio.options.baseUrl = Uri.http(Settings.apiUrl, 'archfolio/v1').toString();
+    int userId = loggedUser.id;
+    var response = await dio.get('/users?id=$userId');
+    User new_user = User.fromJson(jsonDecode(response.toString()));
+    setState(() {
+      loggedUser = new_user;
+    });
+  }
+
+  void _navigateAndDisplaySelection(BuildContext context) async {
+    final new_user = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => EditProfileScreen(loggedUser: loggedUser,)),
+    );
+    setState(() {
+      loggedUser = new_user;
+    });
   }
 }
 
@@ -164,3 +183,5 @@ class _ProfileScreenState extends State<ProfileScreen> {
 //     ),
 //   );
 // }
+
+
